4d84bc5e721767743ed7ebb7ee8adf614a010c9e
dvaiman <daniel.vaisanen@gih.se>
2020-08-09 08:27:52 +0200

    changed background fill

--- 2020/week_32/energy_in_european_countries.R
+++ 2020/week_32/energy_in_european_countries.R
@@ -69,7 +69,7 @@ country_totals %>% janitor::clean_names() %>%
   angle = 0,
   hjust=1,
   vjust=0.5,
-  nudge_y = -6000,
+  nudge_y = -10000,
   family = "Roboto") +
   geom_text(data = ct, mapping=aes(
     x= country_name, 
@@ -80,7 +80,7 @@ country_totals %>% janitor::clean_names() %>%
   angle = 0,
   hjust=0,
   vjust=0.5,
-  nudge_y = 6000,
+  nudge_y = 10000,
   family = "Roboto") +
   
   #Totals
@@ -110,11 +110,11 @@ country_totals %>% janitor::clean_names() %>%
                linejoin = "round",
                arrow = arrow(length = unit(0.2, "inches"))) +
   labs(title = "\n      Energy in European countries\n\n",
-       caption = "\n\n\nData: Eurostat | Graphics: Daniel Väisänen") +
+       caption = "\n\n\nSource: Eurostat | Graphics: Daniel Väisänen") +
   theme_void() +
   theme(plot.margin = unit(c(1,1,1,1), "cm"),
-        plot.background = element_rect(fill="#00324e", color = "#00324e"),
-        panel.background = element_rect(fill="#00324e", color = "#00324e"),
+        plot.background = element_rect(fill="#003f62", color = "#003f62"),
+        panel.background = element_rect(fill="#003f62", color = "#003f62"),
         plot.caption = element_text(angle = 0, vjust = 0.5, hjust = 0.5, family = "Roboto", size = 6)) +
   coord_flip(clip = "off")
 
